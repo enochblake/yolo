781cd50ae13b3f34f9b4b4da99b49914613840f2
enochblake <enochblake9@gmail.com>
2024-10-28 15:11:25 +0300

    Set up stage_two directory with Ansible and Terraform configuration for Google Cloud

---
+++ stage_two/terraform/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  credentials = file("//home/blake/Downloads/genuine-segment-440011-t7-74bf4a4b0b70.json") # Replace with your service account key path
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "app_server" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image_project}/${var.image_family}" # Using the image family and project
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Include this to give the VM an external IP
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${google_compute_instance.app_server.network_interface[0].access_config[0].nat_ip}' > ../ansible/hosts"
+  }
+}
+
+output "instance_ip" {
+  value = google_compute_instance.app_server.network_interface[0].access_config[0].nat_ip
+}
---
+++ stage_two/terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+project_id = "velvety-setup-440011-d9"  // Replace with your actual GCP project ID
+region     = "us-central1"            // You can change the region if desired
+instance_name = "my-ecommerce-app"    // Set your desired instance name
+machine_type = "e2-medium"            // Set your preferred machine type
+zone       = "us-central1-a"          // Change this to your preferred zone
